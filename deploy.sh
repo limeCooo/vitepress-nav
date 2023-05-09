8bcdd49689d822ef7d15e8bb56934d4cbec630d2
chenwenchao <washuya@qq.com>
2023-05-09 16:31:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -19,7 +19,4 @@ git commit -m 'deploy'
 # 如果部署到 https://<USERNAME>.github.io
  git push -f git@github.com:limeCooo/limeCooo.github.io.git master
 
-# 如果是部署到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-
-#cd -
+cd -
